c81396fa93181dfc29aeedaa1176c7631aff9bc0
bmehta <bmehta@gannett.com>
2021-02-01 10:32:49 -0500

    Added authentication.

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,17 @@ import CreateBook from '../components/CreateBook';
 
 const Home = () => {
     const [books, setBooks] = useState([]);
+    const [notification, setNotification] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
+    fire.auth()
+        .onAuthStateChanged((user) => {
+            if (user) {
+                setLoggedIn(true)
+            } else {
+                setLoggedIn(false)
+            }
+        });
+
     useEffect(() => {
         fire.firestore()
             .collection('books')
@@ -18,22 +29,50 @@ const Home = () => {
                 setBooks(books);
             });
     }, []);
+
+    const handleLogout = () => {
+        fire.auth()
+            .signOut()
+            .then(() => {
+                setNotification('Logged out');
+                setTimeout(() => {
+                    setNotification('')
+                }, 2000)
+            });
+    };
     return (
         <div>
             <Head>
                 <title>Binita's book app: Made with React, Next and Firebase</title>
             </Head>
             <h1>Books! A persons best friends</h1>
-            <ul>
-                {books.map(book =>
-                    <li key={book.id}>
-                        <Link href="/book/[id]" as={'/book/' + book.id}>
-                            <a>{book.title}</a>
-                        </Link>
-                    </li>
-                )}
-            </ul>
-            <CreateBook />
+            {notification}
+            {!loggedIn
+                ?
+                <div>
+                    <Link href="/users/register">
+                        <a>Register</a>
+                    </Link> |
+                    <Link href="/users/login">
+                        <a> Login</a>
+                    </Link>
+                </div>
+                :
+                <div>
+                    <button onClick={handleLogout}>Logout</button>
+                    <ul>
+                    {books.map(book =>
+                        <li key={book.id}>
+                            <Link href="/book/[id]" as={'/book/' + book.id}>
+                                <a>{book.title}</a>
+                            </Link>
+                        </li>
+                    )}
+                    </ul>
+                    <CreateBook />
+                </div>
+            }
+
         </div>
     )
 };
