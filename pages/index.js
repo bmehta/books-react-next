74902dca2d8ae72d17b918f720457bd231813ba8
bmehta <bmehta@gannett.com>
2021-02-01 09:39:33 -0500

    Displaying books from and adding books to firestore.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,38 @@
-function HomePage() {
-    return <div>Welcome to Next.js!Yay!</div>
-}
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import fire from '../config/fire-config';
+import CreateBook from '../components/CreateBook';
 
-export default HomePage
+
+const Home = () => {
+    const [books, setBooks] = useState([]);
+    useEffect(() => {
+        fire.firestore()
+            .collection('books')
+            .onSnapshot(snap => {
+                const books = snap.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
+                setBooks(books);
+            });
+    }, []);
+    return (
+        <div>
+            <Head>
+                <title>Binita's book app: Made with React, Next and Firebase</title>
+            </Head>
+            <h1>Books! A persons best friends</h1>
+            <ul>
+                {books.map(book =>
+                    <li key={book.id}>
+                        {book.title}
+                    </li>
+                )}
+            </ul>
+            <CreateBook />
+        </div>
+    )
+};
+
+export default Home;
